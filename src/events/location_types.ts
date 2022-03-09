76c36e05d6fe18683cdde2ac0aec4b2b98d07976
Kerry Archibald <kerrya@element.io>
2022-03-09 10:31:31 +0100

    use Default name for static location
    Signed-off-by: Kerry Archibald <kerrya@element.io>

--- src/events/location_types.ts
+++ src/events/location_types.ts
@@ -44,7 +44,7 @@ import { M_MESSAGE_EVENT_CONTENT } from "./message_types";
 export enum LocationAssetType {
     Self = "m.self",
     Live = "m.self.live",
-    Static = "m.location",
+    Default = "m.location",
 }
 
 export const M_ASSET = new UnstableValue("m.asset", "org.matrix.msc3488.asset");
