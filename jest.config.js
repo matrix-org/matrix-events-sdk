3714496984fb726ec8e2ae14469c2bdebbb6ee49
Travis Ralston <travisr@matrix.org>
2022-11-29 15:13:41 -0700

    Add jest coverage requirements

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ["./src/**"],
+  coverageThreshold: {
+    global: {
+      lines: 100,
+      branches: 100,
+      functions: 100,
+      statements: -1,
+    },
+  },
 };
